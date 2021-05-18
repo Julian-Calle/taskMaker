0c5082d3eb3f9de7ee445050d19390e431c4d468
Ubeira <ubeiram@gmail.com>
2021-05-18 18:24:58 +0200

    front first commit

--- middlewares/index.js
+++
@@ -1,5 +0,0 @@
-const isAuthorized = require("./isAuthorized");
-const ifTaskExists = require("./ifTaskExists");
-const isUser = require("./isUser");
-const isMember = require("./isMember");
-module.exports = { isAuthorized, ifTaskExists, isUser, isMember };
