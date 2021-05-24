d0c9ca912eeb40c9c1f200b964b14259da5dc534
Julian-Calle <jcallecristancho@gmail.com>
2021-05-24 19:13:56 +0200

    5 http  request added

--- front/src/https/tasks.js
+++ front/src/https/tasks.js
@@ -2,16 +2,82 @@ import {
   fetchApi,
   fetchFormData,
   requestMethods,
-} from '../shared/utils/fetchFunctions';
+} from "../shared/utils/fetchFunctions";
 const endpoints = {
-  getAllPacks: '/packs',
+  getAllPacks: "/packs",
+  createTask: "/tasks",
+  TasksTypes: "/tasks/types",
+  sendTaskToOtherUser: "/tasks/send/",
+  shareTaskToOtherUser: "/tasks/share/",
+  kickOutInvitedUSer: "tasks/kickOut/",
 };
 
 export async function getAllPacks() {
   const response = await fetchApi(`${endpoints.getAllPacks}`, {
     method: requestMethods.get,
   });
-  if (response.status === 'ok') {
+  if (response.status === "ok") {
+    return response.data;
+  }
+  console.log(response);
+}
+
+export async function newTask(task, color, type, timeLimit) {
+  const response = await fetchApi(`${endpoints.createTask}`, {
+    method: requestMethods.post,
+    body: { task, color, type, timeLimit },
+  });
+  if (response.status === "ok") {
+    return response.data;
+  }
+  console.log(response);
+}
+
+export async function getTasksTypes() {
+  const response = await fetchApi(`${endpoints.TasksTypes}`, {
+    method: requestMethods.get,
+  });
+  if (response.status === "ok") {
+    return response.data;
+  }
+  console.log(response);
+}
+
+export async function sendTask(invitedUSerId, email, name) {
+  const response = await fetchApi(
+    `${endpoints.sendTaskToOtherUser}${invitedUSerId}`,
+    {
+      method: requestMethods.post,
+      body: { email, name },
+    }
+  );
+  if (response.status === "ok") {
+    return response.data;
+  }
+  console.log(response);
+}
+
+export async function kickOutUser(taskId, invitedUSerId) {
+  const response = await fetchApi(
+    `${endpoints.kickOutInvitedUSer}${taskId}/${invitedUSerId}`,
+    {
+      method: requestMethods.delete,
+    }
+  );
+  if (response.status === "ok") {
+    return response.data;
+  }
+  console.log(response);
+}
+
+export async function shareTaks(taskId, invitedUSerId) {
+  const response = await fetchApi(
+    `${endpoints.shareTaskToOtherUser}${taskId}/${invitedUSerId}`,
+    {
+      method: requestMethods.get,
+    }
+  );
+  if (response.status === "ok") {
     return response.data;
   }
   console.log(response);
