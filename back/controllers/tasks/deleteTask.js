cc8a1e7cee95b23729cbd9d7a7c07a6f580d626e
Ubeira <ubeiram@gmail.com>
2021-05-31 18:50:54 +0200

    modifado deleteTask back

--- back/controllers/tasks/deleteTask.js
+++ back/controllers/tasks/deleteTask.js
@@ -2,23 +2,30 @@ const { createError } = require('../../helpers');
 
 const deleteTask = async (req, res, next) => {
   let connection;
-
+  let message;
   try {
     connection = await req.app.locals.getDB();
 
     //Elimino todos los user de la tabla memberList si es el propietario
     if (req.selectedTask.userId !== req.user.id) {
-      throw createError('Solo puedes borrar la task el propietario');
-    }
-    await connection.query(
-      `
+      await connection.query(
+        `
+        DELETE FROM membersList WHERE taskId = ? AND userId=?;`,
+        [req.selectedTask.id, req.user.id]
+      );
+      message = `Has eliminado tu suscripción a la task ${req.selectedTask.id}`;
+    } else {
+      await connection.query(
+        `
       DELETE FROM membersList WHERE taskId = ?;
       DELETE FROM tasks WHERE (id = ? AND userId= ?);`,
-      [req.selectedTask.id, req.user.id, req.selectedTask.id, req.user.id]
-    );
+        [req.selectedTask.id, req.user.id, req.selectedTask.id, req.user.id]
+      );
+      message = `Has eliminado la task ${req.selectedTask.id}`;
+    }
     res.send({
       status: 'ok',
-      data: `El 'task' con id ${req.selectedTask.id} ha sido borrado`,
+      data: message,
     });
   } catch (error) {
     next(error);
