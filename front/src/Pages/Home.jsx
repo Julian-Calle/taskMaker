e505df84527823ce89349687489675e770e7c00f
Ubeira <ubeiram@gmail.com>
2021-06-07 19:08:15 +0200

    logOut

--- front/src/Pages/Home.jsx
+++ front/src/Pages/Home.jsx
@@ -1,7 +1,6 @@
 import useAuth from '../shared/hooks/useAuth';
 import Login from '../Components/user/Login';
-import Stamp from '../Components/Tasks/Stamp';
-import AddTask from '../Components/Tasks/AddTask';
+import LogOut from '../Components/user/LogOut';
 
 export default function Home() {
   const { signIn } = useAuth();
@@ -9,9 +8,7 @@ export default function Home() {
   return (
     <div>
       <Login onSubmit={signIn} />
-      {/*<Stamp color={'green'} text="DONE" />
-      <Stamp color={'red'} text="ALARM" />*/}
-      {/*<AddTask />*/}
+      <LogOut />
     </div>
   );
 }
